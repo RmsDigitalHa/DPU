a75ee45e56c538665bed9cc8350fb7ad6188d81e
RmsDigitalHa <163966600+RmsDigitalHa@users.noreply.github.com>
2024-03-19 14:34:57 +0900

    Code_1

---
+++ inc/rf_control.h
@@ -0,0 +1,38 @@
+/*
+ * rf_control.h
+ *
+ *  Created on: 2023. 12. 30.
+ *      Author: RMS_Digital
+ */
+
+#ifndef INC_RF_CONTROL_H_
+#define INC_RF_CONTROL_H_
+
+
+#define RCFM 	0
+#define RCRM	1
+
+
+#define 	G_ATTEN_TABLE_SIZE	167
+
+
+typedef struct
+{
+	uint64_t		u64StartFreq;
+	uint8_t			u8AMP1_GAIN_ATTEN;
+	uint8_t			u8AMP2_GAIN_ATTEN;
+	uint8_t		 	u8BYPASS_GAIN_ATTEN;
+	uint8_t		 	u8SYSTEM_ATTEN;
+}
+typTableATTEN;
+
+typTableATTEN 	GainAttenTable[167];
+
+
+int GetRFPathStatus();
+void GetStatusPBIT();
+void GetStatusIBIT();
+void SetGainAtten(uint64_t Freq);
+typTableATTEN 	GetAttenValue(uint64_t TargetFreq);
+
+#endif /* INC_RF_CONTROL_H_ */
